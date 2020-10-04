f79fdfcd7850031d320b59af53a109f77e8ffaea
Mike Irvine <Mike.Irvine@BCRICWH.LAN>
2020-10-04 16:23:27 -0700

    Complete part one script

--- stats_intro/part_one/part_one_script.R
+++ stats_intro/part_one/part_one_script.R
@@ -10,7 +10,9 @@ library("tidyverse")
 6*7
 2^3
 
-# Store values to retrive them later
+# Using the hash symbol so R ignores it
+
+# Store values to retrieve them later
 
 a <- 2*3
 b = 2*3
@@ -19,6 +21,11 @@ b = 2*3
 a == b
 a > b
 a < b 
+
+# last one guess output:
+# TRUE
+# FALSE 
+# logical(0)
 
 # creating a vector of values
 
@@ -29,7 +36,32 @@ values <- c(1,2,3)
 sum(values)
 mean(values)
 
+# introducing the data frame
 
+iris 
 
-as_factor(iris$Species)
+# We can look up any variables or function using the ? operator
+
+?iris
+
+# print the first few rows of a data frame
+
+head(iris,n=3)
+
+# View the whole dataframe
+
+View(iris)
+
+# summarize each column in iris
+
+summary(iris)
+
+# introduce different data types
+
+# What data type is Species?
+
+iris$Species
+
+# Contingency analysis for a nominal variable example
+
 table(iris$Species)
